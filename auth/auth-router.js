31d3c2cf4ecd83e2bc7622247cb976c44c917eab
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-08 12:12:49 -0500

    Auth router passing all tests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
+const authModel = require("./auth-model");
+const bcrypt = require("bcryptjs");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  const {username, password} = req.body;
+  if(!username || !password) return res.status(400).json({message: "Username and password required"});
+  
+  try {
+    const userExists = await authModel.getByUsername(username);
+    if(userExists) return res.status(400).json({message: "Username taken"});
+
+    const user = await authModel.createUser(username, password);
+    if(user) return res.status(201).json(user);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message: "A server error occurred"});
+  }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+
 });
 
 module.exports = router;
